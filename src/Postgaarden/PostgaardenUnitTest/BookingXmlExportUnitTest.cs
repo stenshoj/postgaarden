50e0703e60836588239b9d3a07ead4cbf28bdd3c
stenshoj <c.stenshoj@gmail.com>
2015-11-18 08:58:28 +0100

    Added done XMLExport

--- src/Postgaarden/PostgaardenUnitTest/BookingXmlExportUnitTest.cs
+++ src/Postgaarden/PostgaardenUnitTest/BookingXmlExportUnitTest.cs
@@ -5,6 +5,7 @@ using Postgaarden.Model.Rooms;
 using Postgaarden.Model.Equipments;
 using Moq;
 using System.IO;
+using Postgaarden;
 
 namespace PostgaardenUnitTest
 {
@@ -16,12 +17,16 @@ namespace PostgaardenUnitTest
         {
             string xml = "";
             Booking b = new Booking();
+            b.Id = 1;
+            b.Price = 399;
             b.Employee = new Employee { EmailAddress = "mail@mail.dk", Id = 1, Name="Bo" };
             b.Customer = new Customer { CompanyName = "Ecco", Cvr = "12345678", Name = "Niels", EmailAddress = "niels@mail.dk" };
             b.SetTime(new DateTime(2000, 08, 08), new DateTime(2000, 08, 09));
             Room room = new ConferenceRoom();
-            room.Equipments.Add(new Equipment("Stol"));
-            room.Equipments.Add(new Equipment("Grill"));
+            room.Equipments.Add(new Equipment("Stol") {Id=1});
+            room.Equipments.Add(new Equipment("Grill") {Id=2});
+            room.Id = 1;
+            room.Size = 50;
             b.Room = room;
             var writerMock = new Mock<TextWriter>();
             writerMock.Setup(x => x.Write(It.IsAny<string>())).Callback((string s) => xml = s);
@@ -33,6 +38,38 @@ namespace PostgaardenUnitTest
         }
 
         private string data =
-            "";
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+            "<Booking xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
+                "<Id>1</Id>" +
+                "<Price>399.0</Price>" +
+                "<Startime>2000-08-08T00:00:00</Startime>" +
+                "<EndTime>2000-08-09T00:00:00</EndTime>" +
+                "<Employee>" +
+                    "<Id>1</Id>" +
+                    "<Name>Bo</Name>" +
+                    "<EmailAddress>mail @mail.dk</EmailAddress>" +
+                "</Employee>" +
+                "<Customer>" +
+                    "<CompanyName>Ecco</CompanyName>" +
+                    "<CVR>12345678</CVR>" +
+                    "<Name>Niels</Name>" +
+                    "<EmailAddress>niels @mail.dk</EmailAddress>" +
+                "</Customer>" +
+                "<Room>" +
+                    "<Id>1</Id>" +
+                    "<Size>50</Size>" +
+                    "<Name>Mødelokale 1</Name>" +
+                    "<Equipments>" +
+                        "<Equipment>" +
+                            "<Id>1</Id>" +
+                            "<Name>Stol</Name>" +
+                        "</Equipment>" +
+                        "<Equipment>" +
+                            "<Id>2</Id>" +
+                            "<Name>Grill</Name>" +
+                        "</Equipment>" +
+                    "</Equipments>" +
+                "<Room>" +
+            "</Booking>";
     }
 }
