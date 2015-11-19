60d1d0e154c63aed468f3f182fa70bd5a0247702
Hvolsomt <mhvolh@gmail.com>
2015-11-19 10:45:39 +0100

    Added Administration GUI - Regards

---
+++ src/Postgaarden/PostgaardenGui/Administration/Gui/AdministrationWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Postgaarden.Connection.Sqlite;
+using Postgaarden.Crud.Equipments;
+using Postgaarden.Crud.Rooms;
+using Postgaarden.Model.Rooms;
+
+namespace PostgaardenGui.Administration.Gui
+{
+    /// <summary>
+    /// Interaction logic for AdministrationWindow.xaml
+    /// </summary>
+    public partial class AdministrationWindow : Window
+    {
+        RoomHandler roomHandler;
+
+        public ObservableCollection<string> EquipmentFiltlerObservableCollection { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<Room> RoomObservableCollection { get; set; } = new ObservableCollection<Room>();
+
+        public AdministrationWindow()
+        {
+            InitializeComponent();
+
+            var connection = SqliteDatabaseConnection.GetInstance(Properties.Settings.Default.Postgaarden);
+            var equipmentCrud = new SqliteEquipmentCrud(connection);
+            var roomCrud = new SqliteRoomCrud(connection, equipmentCrud);
+            roomHandler = new RoomHandler(roomCrud);
+        }
+
+        private void RoomOverviewWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void AddFilterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            EquipmentFiltlerObservableCollection.Add(FilterTextTextBox.Text);
+            FilterTextTextBox.Text = "";
+        }
+
+        private void RemoveFilterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            EquipmentFiltlerObservableCollection.Remove((string)EquipmentFilterListBox.SelectedItem);
+        }
+
+        private void FilterTextTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            AddFilterButton.IsEnabled = FilterTextTextBox.Text != "";
+        }
+
+        private void EquipmentFilterListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RemoveFilterButton.IsEnabled = true;
+        }
+
+        private void SearchEquipmentButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var size = SetSizeTextBox.Text.Equals("") ? "0" : SetSizeTextBox.Text;
+            var sizeRooms = new List<Room>(roomHandler.Filter(Convert.ToInt32(size), SetMinimumCheckBox.IsChecked ?? true));
+            var equipmentRooms = new List<Room>(roomHandler.Filter(EquipmentFiltlerObservableCollection));
+            RoomObservableCollection = new ObservableCollection<Room>(sizeRooms.Intersect(equipmentRooms));
+            RoomListBox.ItemsSource = RoomObservableCollection;
+        }
+    }
+}
